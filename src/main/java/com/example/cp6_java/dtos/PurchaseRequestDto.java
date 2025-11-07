ec42a1064c86cec1fbb4885e787d5cc42d233f78
Julia Monteiro <jlmonteiro2355@gmail.com>
2025-11-07 19:20:12 -0300

    Update PurchaseRequestDto.java

--- src/main/java/com/example/cp6_java/dtos/PurchaseRequestDto.java
+++ src/main/java/com/example/cp6_java/dtos/PurchaseRequestDto.java
@@ -1,17 +1,16 @@
 package com.example.cp6_java.dtos;
 
 import java.math.BigDecimal;
-// IMPORTANTE: Adicione as anotações do Lombok se estiver usando!
-// @Data, @NoArgsConstructor, @AllArgsConstructor
+
 
 public class PurchaseRequestDto {
     private String orderId;
     private String buyerEmail;
     private BigDecimal amount;
     private String currency;
-    private String cardNumber; // Para simulação
+    private String cardNumber; 
     private String cardHolder;
     private String expiryDate;
 
-    // Adicione Getters e Setters aqui se não usar Lombok
+    
 }
